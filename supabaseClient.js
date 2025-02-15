5e806bc5a9117c3f51a71e4f28bec1d47a75d1b3
sonykikuma <er.abhasoni@gmail.com>
2025-02-15 21:08:25 +0530

    first commit

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,17 +1,17 @@
-// import { createClient } from "@supabase/supabase-js";
-
-// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-
-// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY || "";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.error("Supabase credentials are missing.");
-}
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// import { createClient } from "@supabase/supabase-js";
+
+// const supabaseUrl = process.env.VITE_SUPABASE_URL || "";
+// const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY || "";
+
+// if (!supabaseUrl || !supabaseAnonKey) {
+//   console.error("Supabase credentials are missing.");
+// }
+
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
